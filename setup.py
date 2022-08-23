10b00d6f83bd5a9ab484ef79284c848dfba8403a
Ayal Lutwak <alutwak@protonmail.com>
2022-08-23 13:45:20 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+requirements = ["digikey-api", "openpyxl", "PyYaml"]
+
+setup(name="Parts",
+      description="A library for managing electronic parts",
+      author="Ayal Lutwak",
+      author_email="ayal@audio-electric.com",
+      platforms=["linux", "mac", "windows"],
+      install_requires=requirements,
+      packages=find_packages(exclude=["__main__"]),
+      entry_points="""
+      [console_scripts]
+      addparts=addparts:main
+      printpart=printpart:main
+      """)
